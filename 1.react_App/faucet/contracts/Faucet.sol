f7e89132d26ff226b457dbc0389a44952393dda2
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-09-25 20:04:58 +0530

    added a first react project

---
+++ 1.react_App/faucet/contracts/Faucet.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity ^0.8.13;
+
+contract Faucet{
+  // storage variables
+  uint public funds = 1000;
+  
+}
